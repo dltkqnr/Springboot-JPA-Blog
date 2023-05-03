8d76627b55c2ea69ab6561605cfd632d3c251a25
dltkqnr <dltkqnr@naver.com>
2023-05-04 00:13:49 +0900

    ajax를 이용한 회원가입

---
+++ src/main/java/com/tkqnr/blog/controller/api/UserApiController.java
@@ -0,0 +1,28 @@
+package com.tkqnr.blog.controller.api;
+
+import com.tkqnr.blog.dto.ResponseDto;
+import com.tkqnr.blog.model.RoleType;
+import com.tkqnr.blog.model.User;
+import com.tkqnr.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/user")
+    public ResponseDto<Integer> save(@RequestBody User user){ //username,password,email
+        System.out.println("UserApiController : save 호출됨");
+
+        user.setRole(RoleType.USER);
+        userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바 Object를 Json으로 변환해서 리턴 (Jackson)
+    }
+}
