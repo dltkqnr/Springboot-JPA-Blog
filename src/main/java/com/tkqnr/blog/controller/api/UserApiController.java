6d1a605e4cb9a82731469e2ae6806d0a2369fb49
dltkqnr <dltkqnr@naver.com>
2023-05-07 23:59:39 +0900

    login

--- src/main/java/com/tkqnr/blog/controller/api/UserApiController.java
+++ src/main/java/com/tkqnr/blog/controller/api/UserApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 
 @RestController
 public class UserApiController {
@@ -25,4 +27,15 @@ public class UserApiController {
         userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바 Object를 Json으로 변환해서 리턴 (Jackson)
     }
+    @PostMapping("/api/user/login")
+    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
+        System.out.println("UserApiController : login 호출됨");
+        User principal = userService.로그인(user); //principal(접근주체)
+
+        if(principal != null){
+            session.setAttribute("principal",principal);
+        }
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
 }
