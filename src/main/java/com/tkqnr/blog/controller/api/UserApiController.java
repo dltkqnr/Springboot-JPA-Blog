bc1a156b4eeaf28dd65471fa4293f294f78c50e5
dltkqnr <dltkqnr@naver.com>
2023-05-09 01:23:12 +0900

    setRole 서비스로 이동

--- src/main/java/com/tkqnr/blog/controller/api/UserApiController.java
+++ src/main/java/com/tkqnr/blog/controller/api/UserApiController.java
@@ -6,6 +6,7 @@ import com.tkqnr.blog.model.User;
 import com.tkqnr.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,10 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
-
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user){ //username,password,email
         System.out.println("UserApiController : save 호출됨");
 
-        user.setRole(RoleType.USER);
         userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바 Object를 Json으로 변환해서 리턴 (Jackson)
     }
