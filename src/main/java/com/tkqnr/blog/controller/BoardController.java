7ab4132239e6b73594874cfa6fd34d21c4770088
dltkqnr <dltkqnr@naver.com>
2023-05-10 17:39:13 +0900

    controller에서 세션 접근

--- src/main/java/com/tkqnr/blog/controller/BoardController.java
+++ src/main/java/com/tkqnr/blog/controller/BoardController.java
@@ -1,14 +1,21 @@
 package com.tkqnr.blog.controller;
 
+import com.tkqnr.blog.config.auth.PrincipalDetail;
+import com.tkqnr.blog.config.auth.PrincipalDetailService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    private PrincipalDetailService principal;
+
+    //@AuthenticationPrincipal PrincipalDetail principal
     @GetMapping({"","/"})
-    public String index(){
+    public String index(@AuthenticationPrincipal PrincipalDetail principal){
         // /WEB-INF/views/index.jsp
+        System.out.println("로그인 사용자 아이디:"+principal.getUsername());
         return "index";
     }
 }
