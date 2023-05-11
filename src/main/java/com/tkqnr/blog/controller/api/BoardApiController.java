1bbe607dc05b9665b11c7b768ab24bec62617a46
dltkqnr <dltkqnr@naver.com>
2023-05-11 22:54:32 +0900

    BoardApiController 생성

---
+++ src/main/java/com/tkqnr/blog/controller/api/BoardApiController.java
@@ -0,0 +1,31 @@
+package com.tkqnr.blog.controller.api;
+
+import com.tkqnr.blog.config.auth.PrincipalDetail;
+import com.tkqnr.blog.dto.ResponseDto;
+import com.tkqnr.blog.model.Board;
+import com.tkqnr.blog.model.User;
+import com.tkqnr.blog.service.BoardService;
+import com.tkqnr.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BoardApiController {
+
+
+    @Autowired
+    private BoardService boardService;
+
+    @PostMapping("/api/board")
+    public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal){ //title, content
+
+        boardService.글쓰기(board, principal.getUser());
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+}
