fb0be9a38e3d1bbfeba1841c09cd80b30ff358df
dltkqnr <dltkqnr@naver.com>
2023-05-13 00:22:56 +0900

    글상세보기 구현(뒤로가기)

--- src/main/java/com/tkqnr/blog/controller/BoardController.java
+++ src/main/java/com/tkqnr/blog/controller/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -25,6 +26,12 @@ public class BoardController {
 
         // /WEB-INF/views/index.jsp
         return "index"; //viewResolver 작동!
+    }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id,Model model){
+        model.addAttribute("board",boardService.글상세보기(id));
+        return "board/detail";
     }
 
     //USER 권한이 필요
