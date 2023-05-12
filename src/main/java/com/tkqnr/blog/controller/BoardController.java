4755077d714a48edee8fcecaa4b5005ec783e8f5
dltkqnr <dltkqnr@naver.com>
2023-05-12 11:12:09 +0900

    글목록 보기 구현

--- src/main/java/com/tkqnr/blog/controller/BoardController.java
+++ src/main/java/com/tkqnr/blog/controller/BoardController.java
@@ -2,20 +2,26 @@ package com.tkqnr.blog.controller;
 
 import com.tkqnr.blog.config.auth.PrincipalDetail;
 import com.tkqnr.blog.config.auth.PrincipalDetailService;
+import com.tkqnr.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
-    private PrincipalDetailService principal;
+    @Autowired
+    private BoardService boardService;
 
     //@AuthenticationPrincipal PrincipalDetail principal
     @GetMapping({"","/"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("boards",boardService.글목록());
+
         // /WEB-INF/views/index.jsp
-        return "index";
+        return "index"; //viewResolver 작동!
     }
 
     //USER 권한이 필요
