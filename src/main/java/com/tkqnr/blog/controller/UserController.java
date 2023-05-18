777e9071f307642f6f831c1177964a88908d7bc1
dltkqnr <dltkqnr@naver.com>
2023-05-18 23:37:20 +0900

    회원정보 수정하기 구현

--- src/main/java/com/tkqnr/blog/controller/UserController.java
+++ src/main/java/com/tkqnr/blog/controller/UserController.java
@@ -19,4 +19,9 @@ public class UserController {
     public String loginForm(){
         return "user/loginForm";
     }
+
+    @GetMapping("/user/updateForm")
+    public String updateFrom(){
+        return "user/updateForm";
+    }
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index={
         $("#btn-save").on("click",()=>{ // funtion(){} 대신 ()=>{} 를 사용. this를 바인딩 하기 위해서 
             this.save();
         });
+        $("#btn-update").on("click",()=>{ // funtion(){} 대신 ()=>{} 를 사용. this를 바인딩 하기 위해서 
+            this.update();
+        });
     },
 
     save:function(){
@@ -33,7 +36,31 @@ let index={
             alert(JSON.stringify(error));
         }); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청!
 
-    }
+    },
+    update:function(){
+         let data = {
+            id:$("#id").val(),
+            password:$("#password").val(),
+            email:$("#email").val()
+         };
+
+         $.ajax({
+             type:"PUT",
+             url:"/user",
+             data:JSON.stringify(data), //http body 데이터 
+             contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지 (MIME)
+             dataType:"json" // 요청을 서버로 해서 응답이 왔을 때 기본적으로 모든것이 문자열 (생긴게 json이라면) =>javascript 오브젝트로 변경해줌 
+ 
+             //회원가입 수행 요청 성공하면 done, 실패하면 fail 실행
+         }).done(function(resp){
+             alert("회원수정이 완료되었습니다.")
+             //console.log(resp);
+             location.href="/";
+         }).fail(function(error){
+             alert(JSON.stringify(error));
+         }); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청!
+ 
+     }
 }
 
 index.init();
