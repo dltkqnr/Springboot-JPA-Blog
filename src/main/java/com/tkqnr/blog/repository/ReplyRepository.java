50325fe4ea87e2d8c1a87f65a8f367132d8912a1
dltkqnr <dltkqnr@naver.com>
2023-05-31 15:20:36 +0900

    댓글 리스트 뿌리기

---
+++ src/main/java/com/tkqnr/blog/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.tkqnr.blog.repository;
+
+import com.tkqnr.blog.model.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply,Integer> {
+}
