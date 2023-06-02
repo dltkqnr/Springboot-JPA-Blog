46d0f5bfa4204b96491974510167c7b01d4df4a9
dltkqnr <dltkqnr@naver.com>
2023-06-02 12:14:49 +0900

    네이티브 쿼리를 이용한 댓글 작성

--- src/main/java/com/tkqnr/blog/controller/api/BoardApiController.java
+++ src/main/java/com/tkqnr/blog/controller/api/BoardApiController.java
@@ -44,7 +44,7 @@ public class BoardApiController {
 
     //데이터를 받을 때 컨트롤러에서 dto를 만들어서 받는게 좋다.
     @PostMapping("/api/board/{boardId}/reply")
-    public ResponseDto<Integer> replySave( @RequestBody ReplySaveRequestDto replySaveRequestDto){
+    public ResponseDto<Integer> replySave(@RequestBody ReplySaveRequestDto replySaveRequestDto){
 
         boardService.댓글쓰기(replySaveRequestDto);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
--- src/main/java/com/tkqnr/blog/service/BoardService.java
+++ src/main/java/com/tkqnr/blog/service/BoardService.java
@@ -67,22 +67,6 @@ public class BoardService {
 
     @Transactional
     public void 댓글쓰기(ReplySaveRequestDto replySaveRequestDto){
-
-        User user = userRepository.findById(replySaveRequestDto.getUserId()).orElseThrow(()->{
-            return new IllegalArgumentException("댓글 쓰기 실패: 유저 id를 찾을 수 없습니다.");
-        }); // 영속화
-
-        Board board = boardRepository.findById(replySaveRequestDto.getBoardId()).orElseThrow(()->{
-            return new IllegalArgumentException("댓글 쓰기 실패: 게시글 id를 찾을 수 없습니다.");
-        }); // 영속화
-
-        Reply reply = Reply.builder()
-                        .user(user)
-                        .board(board)
-                        .content(replySaveRequestDto.getContent())
-                        .build();
-
-
-        replyRepository.save(reply);
+        int result = replyRepository.mSave(replySaveRequestDto.getUserId(),replySaveRequestDto.getBoardId(),replySaveRequestDto.getContent());
     }
 }
